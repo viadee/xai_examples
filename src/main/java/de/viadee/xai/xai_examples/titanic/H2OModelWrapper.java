a47d244017ae43bc32970bb8c9f97a1d689b757c
Tobias Goerke <tobias.goerke@gmail.com>
2019-03-05 13:49:03 +0100

    anchors tutorial is now functional

---
+++ src/main/java/de/viadee/xai/xai_examples/titanic/H2OModelWrapper.java
@@ -0,0 +1,61 @@
+package de.viadee.xai.xai_examples.titanic;
+
+import de.viadee.xai.anchor.adapter.tabular.TabularInstance;
+import hex.genmodel.ModelMojoReader;
+import hex.genmodel.MojoModel;
+import hex.genmodel.MojoReaderBackend;
+import hex.genmodel.MojoReaderBackendFactory;
+import hex.genmodel.easy.EasyPredictModelWrapper;
+import hex.genmodel.easy.RowData;
+import hex.genmodel.easy.exception.PredictException;
+import hex.genmodel.easy.prediction.BinomialModelPrediction;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Imports the H2O model exported from R.
+ * <p>
+ * Contains definitions and necessary configuration
+ */
+class H2OModelWrapper {
+    private final EasyPredictModelWrapper model;
+
+    /**
+     * Imports the .zip file (MOJO) containing all exported materials
+     *
+     * @throws IOException if an error occurs
+     */
+    H2OModelWrapper() throws IOException {
+        final URL zipURL = ClassLoader.getSystemResource("h2oModel.zip");
+        final MojoReaderBackend reader = MojoReaderBackendFactory.createReaderBackend(zipURL,
+                MojoReaderBackendFactory.CachingStrategy.MEMORY);
+        final MojoModel mojoModel = ModelMojoReader.readFrom(reader);
+        model = new EasyPredictModelWrapper(mojoModel);
+    }
+
+    /**
+     * Calls the model for a specific instance.
+     * <p>
+     * Maps the datatypes to match the required definition
+     *
+     * @param instance the instance to obtain a prediction for
+     * @return the prediction
+     */
+    int predict(TabularInstance instance) {
+        final RowData row = new RowData();
+        row.put("Sex", instance.getTransformedValue("Sex"));
+        row.put("CabinGiven", String.valueOf(instance.getTransformedValue("Cabin")).toUpperCase());
+        row.put("Pclass", ((Integer) instance.getTransformedValue("Pclass")).doubleValue());
+        row.put("Age", instance.getTransformedValue("Age"));
+        row.put("SibSp", ((Integer) instance.getTransformedValue("SibSp")).doubleValue());
+        row.put("Fare", instance.getTransformedValue("Fare"));
+        row.put("TicketNum", ((Integer) 0).doubleValue());
+
+        try {
+            return ((BinomialModelPrediction) model.predict(row)).labelIndex;
+        } catch (PredictException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/de/viadee/xai/xai_examples/titanic/TitanicExplanation.java
+++ src/main/java/de/viadee/xai/xai_examples/titanic/TitanicExplanation.java
@@ -7,44 +7,92 @@ import de.viadee.xai.anchor.algorithm.AnchorConstructionBuilder;
 import de.viadee.xai.anchor.algorithm.AnchorResult;
 import de.viadee.xai.anchor.algorithm.global.CoveragePick;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
 
 /**
  * Main entry comprising all steps to create both local and global explanations
  */
 public class TitanicExplanation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Load dataset and its description
-        final AnchorTabular anchorTabular = TitanicDataset.createTabularDefinition();
+        final AnchorTabular anchorTabular = TitanicDataset.createTabularTrainingDefinition();
 
-        // Train a random forest. Required so that there is a model that can be explained by anchors
-        final TabularRandomForestClassifier classifier = TabularRandomForestClassifier
-                .createAndFit(50, anchorTabular.getTabularInstances());
+        // Our first model to be explained (GBM). Imported from an H2O model pre-built and trained in R
+        final H2OModelWrapper h2oModel = new H2OModelWrapper();
+        // Obtain a second suitable model (RandomForest). Train it ourselves this time.
+        final TabularRandomForestClassifier randomForestModel = new TabularRandomForestClassifier(1);
+        randomForestModel.fit(anchorTabular.getTabularInstances());
 
-        // Create default builder which can be used to create anchors
-        final AnchorConstructionBuilder<TabularInstance> defaultBuilder = anchorTabular
-                .createDefaultBuilder(classifier, anchorTabular.getTabularInstances()[0]);
+        // Print the model's test data accuracy
+        outputTestsetAccuracy("H2OModel", h2oModel::predict);
+        outputTestsetAccuracy("RandomForest", randomForestModel);
 
-        // Create a local explanation
-        final AnchorResult<TabularInstance> anchor = defaultBuilder
-                .enableThreading(10, Executors.newFixedThreadPool(10), null)
+        // Pick instance to be explained
+        // Next pick specific instance (countess)
+        final TabularInstance explainedInstance = anchorTabular.getTabularInstances()[0];
+
+        // Create builder that can be used to create an AnchorConstruction instance
+        // H2O default builder
+        final AnchorConstructionBuilder<TabularInstance> h2oBuilder = anchorTabular
+                .createDefaultBuilder(h2oModel::predict, explainedInstance);
+        // RandomForest default builder
+        final AnchorConstructionBuilder<TabularInstance> randomForestBuilder = anchorTabular
+                .createDefaultBuilder(randomForestModel, explainedInstance);
+
+
+        // Create local explanations
+        System.out.println("====H2O Local Explanation====");
+        printLocalExplanationResult(explainedInstance, anchorTabular, h2oBuilder);
+        System.out.println("====Random Forest Local Explanation====");
+        printLocalExplanationResult(explainedInstance, anchorTabular, randomForestBuilder);
+
+        // Create global explanations
+        System.out.println("====H2O Global Explanation====");
+        printGlobalExplanationResult(anchorTabular, h2oBuilder);
+        System.out.println("====Random Forest Global Explanation====");
+        printGlobalExplanationResult(anchorTabular, randomForestBuilder);
+    }
+
+    private static void printLocalExplanationResult(TabularInstance instance, AnchorTabular tabular,
+                                                    AnchorConstructionBuilder<TabularInstance> builder) {
+        final AnchorResult<TabularInstance> anchor = builder
                 .build()
                 .constructAnchor();
 
         System.out.println("====Explained instance====" + System.lineSeparator() +
-                anchorTabular.getVisualizer().visualizeInstance(anchor.getInstance()));
+                tabular.getVisualizer().visualizeInstance(anchor.getInstance()));
 
         System.out.println("====Result====" + System.lineSeparator() +
-                anchorTabular.getVisualizer().visualizeResult(anchor));
-
-        // Use the CoveragePick algorithm to create global explanations
-        final List<AnchorResult<TabularInstance>> globalExplanations = new CoveragePick<>(defaultBuilder, 10,
-                Executors.newCachedThreadPool(), null)
-                .run(anchorTabular.shuffleSplitInstances(1, 0)[0], 20);
-
-        System.out.println(anchorTabular.getVisualizer().visualizeGlobalResults(globalExplanations));
+                tabular.getVisualizer().visualizeResult(anchor));
     }
+
+    private static void printGlobalExplanationResult(AnchorTabular tabular,
+                                                     AnchorConstructionBuilder<TabularInstance> builder) {
+        // Use the CoveragePick algorithm to create global explanations
+        final List<AnchorResult<TabularInstance>> globalExplanations = new CoveragePick<>(builder, 10,
+                Executors.newCachedThreadPool(), null)
+                .run(tabular.getTabularInstances(), 20);
+
+        System.out.println(tabular.getVisualizer().visualizeGlobalResults(globalExplanations));
+    }
+
+
+    private static void outputTestsetAccuracy(String name, Function<TabularInstance, Integer> predictFunction) {
+        final TabularInstance[] testData = TitanicDataset.createTabularTestDefinition().getTabularInstances();
+        final int[] actualTestLabels = TitanicDataset.readTestLabels();
+        int matches = 0;
+        for (int i = 0; i < testData.length; i++) {
+            if (predictFunction.apply(testData[i]).equals(actualTestLabels[i]))
+                matches++;
+        }
+
+        System.out.println("====" + name + " Testset Accuracy====" + System.lineSeparator() +
+                matches / (double) testData.length);
+    }
+
 
 }
