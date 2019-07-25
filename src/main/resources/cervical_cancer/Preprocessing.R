c453ec51788c3ec26199545ae3b5feed61d92c62
Tobias Goerke <tobias.goerke@gmail.com>
2019-07-25 15:49:16 +0200

    Added cervical cancer dataset example

---
+++ src/main/resources/cervical_cancer/Preprocessing.R
@@ -0,0 +1,40 @@
+#==============================================================================
+# Preparing dataset for cervical cancer classification
+#==============================================================================
+# Source:
+# http://archive.ics.uci.edu/ml/datasets/Cervical+cancer+%28Risk+Factors%29
+# Paper: http://www.inescporto.pt/~jsc/publications/conferences/2017KelwinIBPRIA.pdf
+
+library(dplyr)
+library(mlr)
+
+get.cervical.data = function(data_dir){
+  cervical = read.csv(sprintf('%s/risk_factors_cervical_cancer.csv', data_dir), na.strings = c('?'), stringsAsFactors = FALSE)
+  cervical = select(cervical, -Citology, -Schiller, -Hinselmann)
+  cervical$Biopsy = factor(cervical$Biopsy, levels = c(1, 0), labels=c('Cancer', 'Healthy'))
+
+  ## subset variables to the ones that should be used in the book
+  cervical = dplyr::select(cervical, Age, Number.of.sexual.partners, First.sexual.intercourse,
+  Num.of.pregnancies, Smokes, Smokes..years., Hormonal.Contraceptives, Hormonal.Contraceptives..years.,
+  IUD, IUD..years., STDs, STDs..number., STDs..Number.of.diagnosis, STDs..Time.since.first.diagnosis,
+  STDs..Time.since.last.diagnosis, Biopsy)
+
+  # NA imputation
+  imputer = mlr::imputeMode()
+
+
+  cervical_impute = mlr::impute(cervical, classes = list(numeric = imputeMode()))
+  cervical = cervical_impute$data
+  #browser()
+  #cervical = relevel(cervical, "Healthy")
+  cervical
+}
+
+get.cervical.task = function(data_dir){
+  cervical = get.cervical.data(data_dir)
+  mlr::makeClassifTask(id='cervical', data = cervical, target = 'Biopsy')
+}
+
+setwd("C:\\Users\\GoT\\Documents\\git\\xai_examples\\src\\main\\resources\\cervical_cancer")
+data.table::fwrite(get.cervical.data("C:\\Users\\GoT\\Documents\\git\\xai_examples\\src\\main\\resources\\cervical_cancer"), "cervical_cancer_preprocessed.csv")
+                   
